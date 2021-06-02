bc93069f76d8413c97d2a798ea9613fa604b7ea1
Tuan Pham  🐟 <tunaphishy37@gmail.com>
2021-06-02 10:08:05 -0700

    personalized updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `tunaphishy's blog`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Tuan Pham`,
+      summary: `the protagonist`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
-    social: {
-      twitter: `kylemathews`,
-    },
+    description: `tuna's blog`,
+    siteUrl: `https://tunaphish.github.io/blog/`,
   },
   pathPrefix: "/blog",
   plugins: [
@@ -119,7 +116,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tunaphish-logo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
