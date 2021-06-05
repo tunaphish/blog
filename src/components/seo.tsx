eb76bc64073d26320d2e50be2502f416a779da22
Tuan Pham  🐟 <tunaphishy37@gmail.com>
2021-06-04 17:05:59 -0700

    estlint addition WIP

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -5,12 +5,23 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import * as React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+import * as React from 'react';
+import { Helmet } from 'react-helmet';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const Seo = ({ description, lang, meta, title }) => {
+type SeoProps = {
+  description?: string,
+  lang?: string,
+  meta?: any[],
+  title: string,
+};
+
+const Seo = ({
+  description = '',
+  lang = 'en',
+  meta = [],
+  title,
+}: SeoProps) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -24,11 +35,11 @@ const Seo = ({ description, lang, meta, title }) => {
           }
         }
       }
-    `
-  )
+    `,
+  );
 
-  const metaDescription = description || site.siteMetadata.description
-  const defaultTitle = site.siteMetadata?.title
+  const metaDescription = description || site.siteMetadata.description;
+  const defaultTitle = site.siteMetadata?.title;
 
   return (
     <Helmet
@@ -39,53 +50,40 @@ const Seo = ({ description, lang, meta, title }) => {
       titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
       meta={[
         {
-          name: `description`,
+          name: 'description',
           content: metaDescription,
         },
         {
-          property: `og:title`,
+          property: 'og:title',
           content: title,
         },
         {
-          property: `og:description`,
+          property: 'og:description',
           content: metaDescription,
         },
         {
-          property: `og:type`,
-          content: `website`,
+          property: 'og:type',
+          content: 'website',
         },
         {
-          name: `twitter:card`,
-          content: `summary`,
+          name: 'twitter:card',
+          content: 'summary',
         },
         {
-          name: `twitter:creator`,
-          content: site.siteMetadata?.social?.twitter || ``,
+          name: 'twitter:creator',
+          content: site.siteMetadata?.social?.twitter || '',
         },
         {
-          name: `twitter:title`,
+          name: 'twitter:title',
           content: title,
         },
         {
-          name: `twitter:description`,
+          name: 'twitter:description',
           content: metaDescription,
         },
       ].concat(meta)}
     />
-  )
-}
+  );
+};
 
-Seo.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-}
-
-Seo.propTypes = {
-  description: PropTypes.string,
-  lang: PropTypes.string,
-  meta: PropTypes.arrayOf(PropTypes.object),
-  title: PropTypes.string.isRequired,
-}
-
-export default Seo
+export default Seo;
--- src/templates/tags.tsx
+++ src/templates/tags.tsx
@@ -1,25 +1,24 @@
-import React from "react"
-import PropTypes from "prop-types"
-// Components
-import { Link, graphql } from "gatsby"
-const Tags = ({ pageContext, data }) => {
-  const { tag } = pageContext
-  const { edges, totalCount } = data.allMarkdownRemark
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+
+const Tags = ({ pageContext, data }: any) => {
+  const { tag } = pageContext;
+  const { edges, totalCount } = data.allMarkdownRemark;
   const tagHeader = `${totalCount} post${
-    totalCount === 1 ? "" : "s"
-  } tagged with "${tag}"`
+    totalCount === 1 ? '' : 's'
+  } tagged with "${tag}"`;
   return (
     <div>
       <h1>{tagHeader}</h1>
       <ul>
         {edges.map(({ node }) => {
-          const { slug } = node.fields
-          const { title } = node.frontmatter
+          const { slug } = node.fields;
+          const { title } = node.frontmatter;
           return (
             <li key={slug}>
               <Link to={slug}>{title}</Link>
             </li>
-          )
+          );
         })}
       </ul>
       {/*
@@ -28,31 +27,10 @@ const Tags = ({ pageContext, data }) => {
             */}
       <Link to="/tags">All tags</Link>
     </div>
-  )
-}
-Tags.propTypes = {
-  pageContext: PropTypes.shape({
-    tag: PropTypes.string.isRequired,
-  }),
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      totalCount: PropTypes.number.isRequired,
-      edges: PropTypes.arrayOf(
-        PropTypes.shape({
-          node: PropTypes.shape({
-            frontmatter: PropTypes.shape({
-              title: PropTypes.string.isRequired,
-            }),
-            fields: PropTypes.shape({
-              slug: PropTypes.string.isRequired,
-            }),
-          }),
-        }).isRequired
-      ),
-    }),
-  }),
-}
-export default Tags
+  );
+};
+
+export default Tags;
 export const pageQuery = graphql`
   query($tag: String) {
     allMarkdownRemark(
@@ -73,4 +51,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
